21d6883356834b38c80a3a7e82b6c72d19f77cbb
mos <mos.cheele+github@gmail.com>
2022-04-07 21:00:31 +0200

    Setup Spring Boot + Readme

---
+++ src/test/java/com/mos/kafka/kafkaguessnumber/KafkaGuessNumberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mos.kafka.kafkaguessnumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaGuessNumberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
